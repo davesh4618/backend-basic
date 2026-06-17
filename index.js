7a734399860f322d05071bdf1711ef68cbc42470
devesh singh rathor <deveshsinghds7719546@gmail.com>
2026-06-17 16:12:16 +0530

    req and res revision

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!   .......');
+   console.log(req.query);
+  console.log(req.method);
+
 });
 app.get('/twitter', (req, res) => {
   res.send('Hello Devesh!   .......');
+  console.log(req.query);
+  console.log(req.method);
 });
+
+app.get('/login',(req ,res)=>{
+  res.send('<h1>heloooo baby girl</h1>')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
